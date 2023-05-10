dfdc3d1488a57355993ce857c05dfa30053aeb93
smokethis <64678625+smokethis@users.noreply.github.com>
2023-05-10 19:21:14 +0100

    woo

--- lib/notion_controller.py
+++ lib/notion_controller.py
@@ -1,8 +1,10 @@
 """Module for interacting with the Notion API."""
 
 import os
-import json
+import time
 import requests
+from typing import TypedDict
+
 
 NOTION_KEY = os.environ.get("NOTION_KEY")
 headers = {
@@ -12,6 +14,61 @@ headers = {
 }
 
 
+class NotionTimeMachinePerson(TypedDict):
+    """Class for a Notion Time Machine person.
+    Properties:
+        Brief_Biography (str): A brief biography of the person.
+        Notable_Figure (bool): Whether the person is a notable figure.
+        Notes (str): Notes about the person.
+        Dates_Approx (bool): Whether the dates are approximate.
+        Branch (str): The branch of the family tree.
+        Spouse (str): The spouse of the person.
+        Images (str): Images of the person.
+        Parents (str): The parents of the person.
+        Siblings (str): The siblings of the person.
+        Library (str): The library of the person.
+        Children (str): The children of the person.
+        Siblings (str): The siblings of the person.
+        Place_of_Death_Burial (str): The place of death or burial of the person.
+        Tags (str): Tags for the person.
+        Associated_With (str): People associated with the person.
+        Display_Name (str): The display name of the person.
+        Nickname (str): The nickname of the person.
+        Place_of_Birth (str): The place of birth of the person.
+        Gender (str): The gender of the person. (M/F)
+        Birth_Death (str): The birth and death dates of the person.
+        Marriage (str): The marriage date of the person.
+        Title (str): The title of the person.
+        Alt_Names (str): The alternate names of the person.
+        Full_Name (str): The full name of the person.
+    """
+
+    Brief_Biography: str
+    Notable_Figure: bool
+    Notes: str
+    Dates_Approx: bool
+    Branch: str
+    Spouse: str
+    Images: str
+    Parents: str
+    Siblings: str
+    Library: str
+    Children: str
+    Siblings: str
+    Place_of_Death_Burial: str
+    Tags: str
+    Associated_With: str
+    Display_Name: str
+    Nickname: str
+    Place_of_Birth: str
+    Gender: str
+    Birth_Death: str
+    Marriage: str
+    Title: str
+    Alt_Names: str
+    Full_Name: str
+
+
 def search_databases() -> str:
     """Search for databases using a Notion integration.
     Returns:
@@ -19,15 +76,26 @@ def search_databases() -> str:
     # Define search parameters
     search_params = {"filter": {"value": "database", "property": "object"}}
     # Make the request
-    try:
-        search_response = requests.post(
-            "https://api.notion.com/v1/search",
-            json=search_params,
-            headers=headers,
-            timeout=15,
-        )
-    except requests.exceptions.HTTPError as err:
-        print(err)
+    while True:
+        try:
+            search_response = requests.post(
+                "https://api.notion.com/v1/search",
+                json=search_params,
+                headers=headers,
+                timeout=15,
+            )
+        except requests.exceptions.HTTPError as err:
+            print(err)
+        if search_response.status_code == 429:
+            print(
+                "The Notion API is rate-limited. Trying again in {} seconds....".format(
+                    search_response.headers["Retry-After"]
+                )
+            )
+            time.sleep(int(search_response.headers["Retry-After"]))
+        else:
+            break
+
     # print(search_response.json())
     # Get the database ID
     database_id = search_response.json()["results"][0]["id"]
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 """Main function"""
 
 import lib.notion_controller as notion_controller
+from lib.notion_controller import NotionTimeMachinePerson
+from lib.ged4py import GedcomReader
 
-db_id = notion_controller.search_databases()
-# get_db(db_id)
+
+# db_id = notion_controller.search_databases()
+# notion_controller.get_db(db_id)
 # print("============")
-# query_db(db_id)
-notion_controller.post_dbpage_update(db_id)
+# notion_controller.query_db(db_id)
+# notion_controller.post_dbpage_update(db_id)
+
+PATH = "Sample.ged"
+with GedcomReader(PATH) as parser:
+    # GedcomReader provides context support
+    for i, indi in enumerate(parser.records0("INDI")):
+        print(indi)
+        thisperson = NotionTimeMachinePerson()
+        thisperson.set_property_value("Full Name", indi.name.format())
+        thisperson.set_property_value("Birth Date", indi.sub_tag_value("BIRT/DATE"))
+        thisperson.set_property_value("Place of Birth", indi.sub_tag_value("BIRT/PLAC"))
+        thisperson.set_property_value("Death Date", indi.sub_tag_value("DEAT/DATE"))
+        thisperson.set_property_value(
+            "Place of Death / Burial", indi.sub_tag_value("DEAT/PLAC")
+        )
+        thisperson.set_property_value("Gender", indi.sex)
